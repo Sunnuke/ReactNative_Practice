66a13946573a97a6c6e5d59b1359a827b0c3d9c2
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-05-10 08:53:22 -0700

    react-native Hello World app made in expo.io

---
+++ Exercise_Files/Ch02/02_02/App.js
@@ -0,0 +1,13 @@
+// This code was typed into Expo Snack.
+// You can view this running here: https://snack.expo.io/@sunnuke/442f1b
+
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return(
+    <View style={{ padding: 50 }}>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
